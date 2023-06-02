0fa22dd189f1c3f4318c59d8d9614d5b71534226
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-02 18:41:35 +0300

    My first commit!

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,31 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        if (numberToSkip > lastInRow) {
+            System.out.println("number to skip is bigger then the last");
+        } else if (lastInRow < 0) {
+            System.out.println("last number in row is negative");
+        } else {
+            int skippedSum = calculateSkippedSum(numberToSkip);
+            int countedSum = calculateCountedSum(numberToSkip, lastInRow);
+            System.out.println("skipped sum is " + skippedSum);
+            System.out.println("counted sum is " + countedSum);
+        }
+    }
 
+    private int calculateSkippedSum(int numberToSkip) {
+        int sum = 0;
+        for (int i = 1; i <= numberToSkip; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    private int calculateCountedSum(int numberToSkip, int lastInRow) {
+        int sum = 0;
+        for (int i = numberToSkip + 1; i <= lastInRow; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
